2e57f0cb9f3b39cc9125d15559d334ee159b8898
Oliver Kopp <kopp.dev@gmail.com>
2026-02-21 20:26:46 +0100

    Use specific icon

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ rm -rf FooClient/
 
 "$JAVA_HOME/bin/javac" FooClient.java
 "$JAVA_HOME/bin/jar" cfe fooclient.jar FooClient FooClient.class
-"$JAVA_HOME/bin/jpackage" --type app-image --input . --main-jar fooclient.jar --main-class FooClient --verbose
+"$JAVA_HOME/bin/jpackage" --type app-image --input . --main-jar fooclient.jar --main-class FooClient --verbose --resource-dir .
