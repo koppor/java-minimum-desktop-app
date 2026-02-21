27e37c1ad01ddc5d2038cddb889e2c818c98e6b8
Oliver Kopp <kopp.dev@gmail.com>
2026-02-21 20:28:40 +0100

    Use "JavaApp.png"

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@ set -ex
 
 # Remove build directory
 rm -rf FooClient/
+rm -rf tmp/
+mkdir tmp
 
 "$JAVA_HOME/bin/javac" FooClient.java
 "$JAVA_HOME/bin/jar" cfe fooclient.jar FooClient FooClient.class
-"$JAVA_HOME/bin/jpackage" --type app-image --input . --main-jar fooclient.jar --main-class FooClient --verbose --resource-dir .
+"$JAVA_HOME/bin/jpackage" --type app-image --input . --main-jar fooclient.jar --main-class FooClient --verbose --temp tmp/
