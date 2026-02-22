d21ac06141ef404169b4f7909aa54e3dfd560496
Oliver Kopp <kopp.dev@gmail.com>
2026-02-22 13:56:32 +0100

    Add two images

--- build.sh
+++ build.sh
@@ -7,4 +7,5 @@ mkdir tmp
 
 "$JAVA_HOME/bin/javac" FooClient.java
 "$JAVA_HOME/bin/jar" cfe fooclient.jar FooClient FooClient.class
-"$JAVA_HOME/bin/jpackage" --type deb --input . --main-jar fooclient.jar --main-class FooClient --verbose --temp tmp/ --resource-dir res/
+"$JAVA_HOME/bin/jpackage" --type app-image --input . --main-jar fooclient.jar --main-class FooClient --verbose --temp tmp/ --resource-dir res/ --icon res/FooClient.png
+"$JAVA_HOME/bin/jpackage" --type deb --app-image FooClient/ --verbose  --temp tmp/ --icon res/two.png
