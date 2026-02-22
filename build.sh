449c29210110397148084e197ed6c79b4d6d3d08
Oliver Kopp <kopp.dev@gmail.com>
2026-02-22 10:15:08 +0100

    Fix parameters

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ mkdir tmp
 
 "$JAVA_HOME/bin/javac" FooClient.java
 "$JAVA_HOME/bin/jar" cfe fooclient.jar FooClient FooClient.class
-"$JAVA_HOME/bin/jpackage" --type app-image --input . --main-jar fooclient.jar --main-class FooClient --verbose --temp tmp/
+"$JAVA_HOME/bin/jpackage" --type deb --input . --main-jar fooclient.jar --main-class FooClient --verbose --temp tmp/ --resource-dir res/
