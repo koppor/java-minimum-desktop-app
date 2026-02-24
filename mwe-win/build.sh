f08efeda91bc4bdd062d19c20a3a81e1f994ef38
Siedlerchr <siedlerkiller@gmail.com>
2026-02-24 20:02:15 +0100

    add wix file for overriding and add temp dir

--- mwe-win/build.sh
+++ mwe-win/build.sh
@@ -10,4 +10,4 @@ JAVA_HOME="${JAVA_HOME//\\//}"
 "$JAVA_HOME/bin/javac" FooClient.java
 "$JAVA_HOME/bin/jar" cfe fooclient.jar FooClient FooClient.class
 "$JAVA_HOME/bin/jpackage" --type app-image --input . --main-jar fooclient.jar --main-class FooClient --verbose --resource-dir res/
-"$JAVA_HOME/bin/jpackage" --type msi --app-image FooClient/ --verbose --resource-dir res/
+"$JAVA_HOME/bin/jpackage" --type msi --app-image FooClient/ --verbose --resource-dir res/ --temp tmp
