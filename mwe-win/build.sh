8cbfbba96f9a8b406918c78a8f5b9cf55ade93c4
Oliver Kopp <kopp.dev@gmail.com>
2026-02-24 16:18:30 +0100

    Add initial windows build

---
+++ mwe-win/build.sh
@@ -0,0 +1,10 @@
+set -ex
+
+# Remove build directory
+rm -rf FooClient/
+rm -rf tmp/
+mkdir tmp
+
+"$JAVA_HOME/bin/javac" FooClient.java
+"$JAVA_HOME/bin/jar" cfe fooclient.jar FooClient FooClient.class
+"$JAVA_HOME/bin/jpackage" --type app-image --input . --main-jar fooclient.jar --main-class FooClient --verbose --temp tmp/ --resource-dir res/ "$JAVA_HOME/bin/jpackage" --type msi --app-image FooClient/ --verbose  --temp tmp/ --resource-dir res/
