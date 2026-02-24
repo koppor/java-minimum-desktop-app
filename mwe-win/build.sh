adc2cba2844ea40cb292e4699c79b22e2fabad10
Oliver Kopp <kopp.dev@gmail.com>
2026-02-24 16:28:22 +0100

    Fix path

--- mwe-win/build.sh
+++ mwe-win/build.sh
@@ -5,6 +5,8 @@ rm -rf FooClient/
 rm -rf tmp/
 mkdir tmp
 
+JAVA_HOME="${JAVA_HOME//\\//}"
+
 "$JAVA_HOME/bin/javac" FooClient.java
 "$JAVA_HOME/bin/jar" cfe fooclient.jar FooClient FooClient.class
 "$JAVA_HOME/bin/jpackage" --type app-image --input . --main-jar fooclient.jar --main-class FooClient --verbose --temp tmp/ --resource-dir res/ "$JAVA_HOME/bin/jpackage" --type msi --app-image FooClient/ --verbose  --temp tmp/ --resource-dir res/
